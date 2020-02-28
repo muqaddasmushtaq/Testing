82784feb6d890b868a6b2546852e2a72f70bf23a
muqaddasmushtaq <61595098+muqaddasmushtaq@users.noreply.github.com>
2020-02-28 10:40:03 -0700

    While loop

--- test.cpp
+++ test.cpp
@@ -3,7 +3,48 @@ using namespace std;
 
 int main()
 {
-	  //Making some Changes.
-	
-	
+	//Start with the loops.
+	//Loop wahan use hote hen jab hum ksi same code ko bar bar chlana chate hon..
+	//on some condition..
+	//like  if  x <  100   print  x..
+	//is tarha ke condition me...
+	//first loop is while loop..
+	//while loop us waqt tak chalta ha jab tak condition true rahte ha..
+	//
+	//it starts with   while   keyword..  then condition  in ()
+	//and  body of the loop  in  {}
+	int x = 0;
+	while (x < 100)
+	{
+		cout << x << endl;
+		x++;
+		//remember here.. x ko har bar increase krna hoga.. ne to kabi b conditon
+		//false ne hoge or progeamme chalta he rahe ga..
+	}
+	//smaj aye?   ye jo coding hm krty hain kesy pta chlta hai k yhan ye likhna jaisy
+	//cout<<....   kuch basic yad krne pareen gay.. baqi  documentation se dekh kr..
+	//ya jab ye course pura hoga to smaj aa jaye ge...  hum khud b bnate hen ase bhoot sare
+	//cheezeen..
+	//or iowo basic kahan sy yad kron
+	//
+	//yehi jo bta raha hon abe...   phale to if statement btaye..  ab ye while loop and next..
+	//cin  and cout...while loop jb b lgayn gyy yhiii condition lgy gii..  no  jo b condition
+	//zrorat ho ge wo lage ge..  jab program bhara hoga to ye b smaj aa jaye ge...okk
+	//condition can be anything...
+	while (x > 0)
+	{
+		cout << x << endl;
+		x--;
+		//ab ye x ko decrease krta rahe ga..  and condition is
+		// while x  grater.. than   zero
+		// phale loop me 100 tak gia tha..  ab reverse...
+		// ok?ye sb kch c++ mai use hota..  Kia?while loop wgera  sab languages me.. likhne ka style ye C++ ka ha..  har language me
+		// likha thor differentely jata ha... basic cheez same he ha...
+		// abhiii hm cs201 kr rhy.... yes...  in examples ko dekh lena..
+	}
+
+
+	//next is do-while loop...kl kr lyn plzz    okay..kl 7 bjy theek hai
+//cs201 mai c++ language hi hai...yes mt...lb cs201 krny sy c++ a jaye giii  yes.. java ha lakin about 6 ya 7 semester me..
+//6thsemester mai java ho gii 
 }
