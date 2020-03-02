f0edaeba18f76c2794266d874d5840e133106828
muqaddasmushtaq <61595098+muqaddasmushtaq@users.noreply.github.com>
2020-03-02 10:29:46 -0700

    Do while loop..ADDED

--- test.cpp
+++ test.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
-using namespace std;
+#include "Header.h"
+//This is not for you now...  just for managing code..
 
 int main()
 {
+	dowhile();
 	//Start with the loops.
 	//Loop wahan use hote hen jab hum ksi same code ko bar bar chlana chate hon..
 	//on some condition..
@@ -46,5 +47,29 @@ int main()
 
 	//next is do-while loop...kl kr lyn plzz    okay..kl 7 bjy theek hai
 //cs201 mai c++ language hi hai...yes mt...lb cs201 krny sy c++ a jaye giii  yes.. java ha lakin about 6 ya 7 semester me..
-//6thsemester mai java ho gii 
+//6thsemester mai java ho gii
+	system("pause");
+	//used to stop program 
+}
+
+void dowhile()
+{
+	//Do-While loop..
+	// ye b while loop jesa he ha.. farq sirf itna ha while loop phale condition check
+	// krta ha phir body execute hote ha us ke..  do-while loop me phale body execute hote ha
+	// phir condition check hote ha..  ye 1 ya ziada dafa chalta ha..
+	// do  keyword se start hota ha phir us ke body and end me condition..
+	// is me condition k bad semicolon b lagta ha..
+	//
+	int x = 0;
+	do
+	{
+		cout << x++ << endl;
+		//ye x++ post increment ha..  ye phale x ko print kre ga phir usy increment kre ga..
+		// ++x  pre increment x ko increase krne k bad print krta ha..increment mtlb.. value
+		// increase krna..  1 add ho jaye ga is me..okk decrement me substract hota ha  x--  or --x  etc;
+	} while (x < 10);
+
+	//okay? bss debug kr diaa  debug to dikhane k liey kia   kase chalta ha..program ho gya finish
+	//program to folder me create hota ha  .exe file..
 }
